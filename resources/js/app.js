50f3c445855090915e3bf7b5423015a6c1d58010
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-10-22 08:04:45 -0500

    fix: The view and layout of index in categories was created

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,11 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+
+const menuToggle = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+
+menuToggle.addEventListener('click', function () {
+    mobileMenu.classList.toggle('hidden');
+});
