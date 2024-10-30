da08365e2d3f1c37fdbd7808f2fd25b39bdfadc3
Javier Combita Tellez <javiercombita2014@gmail.com>
2024-10-29 19:49:34 -0500

    feat: the role seeder was created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(CategorySeeder::class);
+        $this->call(RoleSeeder::class);
         Product::factory(50)->create();
         // User::factory(10)->create();
 
